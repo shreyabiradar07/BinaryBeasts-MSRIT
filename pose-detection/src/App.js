084864c21b6a60ddedc65b855ded087958e4da82
Shreya Biradar <biradarshreya24@gmail.com>
2021-11-13 16:05:03 +0530

    feat: Pose Detector

---
+++ pose-detection/src/App.js
@@ -0,0 +1,6 @@
+import PoseDetector from "./components/PoseDetector";
+import React from "react";
+
+export default function App() {
+  return <PoseDetector/>;
+}
---
+++ pose-detection/src/components/PoseDetector.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PoseNet from "react-posenet";
+
+export default function PoseDetector() {
+ return (
+     <PoseNet />
+ );
+}
